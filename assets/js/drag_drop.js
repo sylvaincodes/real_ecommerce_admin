6e7dc69cd55026ae8f2d09b4ce9002d3b92f6e6e
sylvaincodes <adandesylvain2010@gmail.com>
2022-12-05 09:05:02 +0100

    Integration de la page d'acceuil

---
+++ assets/js/drag_drop.js
@@ -0,0 +1,15 @@
+/* Drag and drop  */
+
+// const item_drag= document.querySelector('.card__item');
+
+// item_drag.addEventListener('dragstart', dragStart);
+
+// function dragStart(e) {
+//     console.log("drag start");
+//     e.dataTransfer.setData('text/plain',e.target.id);
+
+//     setTimeout(() => {
+//         e.target.classList.add('hide');
+//     }, 0);
+
+// }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,30 @@
 // DROPDOWN
 const btn_dropdown = document.querySelector('.dropdown__btn');
 const dropdown_content = document.querySelector('.dropdown__content');
+const overlay = document.querySelector('.overlay');
+const menu__item = document.querySelectorAll('.menu__item');
+const search__input = document.querySelector('#search__input');
+search__input.focus();
+
+
+// MENU DEROULANT
+menu__item.forEach(element => {
+
+    element.addEventListener('click' , function() {
+        menu__item.forEach(element2 => {
+            if (element2.matches('.active')) {       
+                element2.classList.remove('active');
+            }            
+        })
+        element.classList.add('active');
+    })
+});
+
 
 btn_dropdown.addEventListener('click', function () {
     dropdown_content.classList.toggle('active');
 })
+
 
 // MENU FIXED
 const btn_menufixed = document.querySelector('.btn__menufixed');
@@ -13,18 +33,29 @@ const close__menufixed = document.querySelector('.close__menufixed');
 
 btn_menufixed.addEventListener('click', function () {
     menu__fixed.classList.toggle('active');
+    overlay.classList.add('active');
 })
 
 close__menufixed.addEventListener('click', function () {
     menu__fixed.classList.toggle('active');
+    closeMenuFixed();
 })
+
+function closeMenuFixed() {
+    menu__fixed.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+// OVERLAY
+// overlay.addEventListener('click', function () {
+//     dropdown_content.classList.remove('active');
+// })
 
 
 // SEARCH OPEN
 const btn_search = document.querySelector('.search__btn');
 const search_content = document.querySelector('.search__content');
 const close_search = document.querySelector('.search__content .close-btn');
-const overlay = document.querySelector('.overlay');
 
 btn_search.addEventListener('click', function () {
     search_content.classList.toggle('active');
@@ -32,13 +63,16 @@ btn_search.addEventListener('click', function () {
 
 close_search.addEventListener('click', function () {
     search_content.classList.toggle('active');
+    search__input.focus();
 })
 
-// ADD MORE INPUT
-const add__more = document.querySelector('.add__more');
-const tab_item_form = document.getElementsByClassName('input-group');
 
-add__more.addEventListener('click', function () {
-    tab_item_form.append('dzdzdz');
+document.addEventListener('click', function (event) {
+    if (event.target.matches('.close__menufixed') || event.target.matches('.btn__menufixed')  ||  event.target.matches('.menu__fixed')) {
+        console.log("zezsssssssss");
+    }else{
+        closeMenuFixed()
+    }
+    console.log(event.target.parentNode);
+    
 })
-
